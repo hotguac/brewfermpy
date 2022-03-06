efc5a91cce28eeef00daaaa5bc60f02cf10cbeb6
Joe Kokosa <jkokosa@gmail.com>
2022-03-06 17:56:03 -0500

    Add javascript for automatic refresh webpage

--- webserver/webs.py
+++ webserver/webs.py
@@ -4,10 +4,10 @@
 import logging
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Import application libraries ------------------------------------------------
-sys.path.append('..')
+sys.path.append('..')  # noqa: E402
 import paths
 from xchg import XchgData
 
@@ -21,16 +21,39 @@ xd = XchgData()
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/values')
+def values():
     current = xd.get(paths.beer_temp)
+    target = xd.get(paths.beer_target)
+    chamber = xd.get(paths.chamber_temp)
     sg = xd.get(paths.blue_sg)
 
     templateData = {
         'title': 'Brewferm Controller',
         'current': str(round(current, 1)),
+        'target': str(round(target, 1)),
+        'chamber': str(round(chamber, 1)),
         'sg': sg
     }
+    return templateData
+
+
+@app.route('/')
+def index():
+    current = xd.get(paths.beer_temp)
+    target = xd.get(paths.beer_target)
+    chamber = xd.get(paths.chamber_temp)
+    sg = xd.get(paths.blue_sg)
+
+    templateData = {
+        'title': 'Brewferm Controller',
+        'current': str(round(current, 1)),
+        'target': str(round(target, 1)),
+        'chamber': str(round(chamber, 1)),
+        'sg': sg,
+        'values_url': request.host_url
+    }
+
     return render_template('index.html', **templateData)
 
 
@@ -38,6 +61,7 @@ def index():
 
 if __name__ == '__main__':
     try:
+        logging.debug("Inside main")
         app.run(debug=False, host='0.0.0.0', port=8000)  # port=80 requires sudo to run!!
     except Exception as e:
         logging.exception("Some other error %s %s", type(e), e)
